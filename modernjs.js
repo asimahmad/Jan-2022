984e39691e131a19b9e3bc6f68adc57c0b862d79
Shakeel, Asim <Asim.Shakeel-ext@bshg.com>
2022-01-05 13:52:05 +0530

    use strict and variables

---
+++ modernjs.js
@@ -0,0 +1,19 @@
+'use strict'
+console.log("Using strict mode");
+console.log('Use strict is already automatically enabled in the js now a days');
+
+
+// Variables
+
+let a;
+a = 'Asim';
+console.log(a);
+
+let b = "Simrat";
+console.log(b);
+
+
+let user = "Kriti",
+    age = 23,
+    dept = 'CSE';
+console.log(user);
